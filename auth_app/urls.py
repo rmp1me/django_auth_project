819733ff8a0e48f8818c188cd3f43d7b1e9741ea
rmp1me <117305150+rmp1me@users.noreply.github.com>
2025-07-08 22:35:42 +0530

    Login functionality properly..!

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -4,5 +4,5 @@ from .views import home, register, login_me
 urlpatterns = [
     path('', home, name='home'), 
     path('register/', register, name='register'), 
-    path('login/',login_me,name='login_me')
+    path('login/',login_me,name='login')
 ]
